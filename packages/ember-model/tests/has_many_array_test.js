e115ff4fc81fdbd6e6d76b4588454b2ae5ae6335
Christina Kung <blurei@gmail.com>
2015-10-28 22:30:52 -0400

    Add tests for isEmpty

--- packages/ember-model/tests/has_many_array_test.js
+++ packages/ember-model/tests/has_many_array_test.js
@@ -1,25 +1,24 @@
-//module("Ember.HasManyArray");
-//
-//
-////var Carrot =  Ember.Model.extend({
-////  size: Ember.attr(String),
-////  brands: Ember.attr()
-////});
-////
-////var Bunny =  Ember.Model.extend({
-////  carrots:Ember.hasMany(Carrot, {embedded: true})
-////});
-//
-//test("it exists", function() {
-//  ok(Ember.HasManyArray);
-//});
-//
-////test("trimEmptyRecords removes empty objects from itself", function() {
-////  var bunny = Bunny.create();
-////  bunny.get('carrots').pushObject(Carrot.create({size:"baby", brands:[]}));
-////  bunny.get('carrots').pushObject(Carrot.create({size:"", brands:['farms']}));
-////  bunny.get('carrots').pushObject(Carrot.create({size:"", brands:[]}));
-////  var bunnyJson = bunny.toJSON();
-////  equal(bunnyJson.carrots.length, 2);
-////});
-//
+module("Ember.HasManyArray");
+
+var Carrot =  Ember.Model.extend({
+  size: Ember.attr(String),
+  brands: Ember.attr()
+});
+
+var Bunny =  Ember.Model.extend({
+  carrots:Ember.hasMany(Carrot, {embedded: true})
+});
+
+test("it exists", function() {
+  ok(Ember.HasManyArray);
+});
+
+test("trimEmptyRecords removes empty objects from itself", function() {
+  var bunny = Bunny.create();
+  bunny.get('carrots').pushObject(Carrot.create({size:"baby", brands:[]}));
+  bunny.get('carrots').pushObject(Carrot.create({size:"", brands:['farms']}));
+  bunny.get('carrots').pushObject(Carrot.create({size:"", brands:[]}));
+  var bunnyJson = bunny.toJSON();
+  equal(bunnyJson.carrots.length, 2);
+});
+
